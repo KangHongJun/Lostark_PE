bcbf9fbf29025b6b18ccbd1f8b9152da4b267b63
KangHongJun <40516203+KangHongJun@users.noreply.github.com>
2023-01-05 15:42:34 +0900

    linking curl to use api

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include <QApplication>
 #include "VProductList.h"
 #include "widget.h"
-
-#include <boost/filesystem.hpp>
 #include <iostream>
+
+#include <curl/curl.h>
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+
+
+
     QApplication a(argc, argv);
-    //VProductList t;
-    //t.show();
+    VProductList t;
+    t.show();
 
 
-
-    widget wid;
-    wid.show();
+    //widget wid;
+    //wid.show();
 
     return a.exec();
 }
